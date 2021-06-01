683930fb426038381192b25bb68f0bf062cb7199
olliejohnson <oliver.joseph.johnson@gmail.com>
2021-05-31 20:13:28 -0400

    all the code

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const passport = require("passport");
+const app = express();
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io");
+const jade = require("ejs");
+app.use(express.static("public"));
+app.engine('html', require('ejs').renderFile);
+
+
+app.post("/login", passport.authenticate("local"), function(req, res){
+    res.redirect("/users/" + req.user.username);
+});
+
+
+app.get("/", (req, res) => {
+    res.render('index.html');
+});
+
+app.get("/login", (req, res) => {
+    res.render('login.html');
+});
+
+app.listen(80);
