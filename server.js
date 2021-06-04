a60b56437e43b38ad2e98f5a6378fc62ec4aefe7
olliejohnson <oliver.joseph.johnson@gmail.com>
2021-06-03 20:11:27 -0400

    fix

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-const express = require("express");
-const passport = require("passport");
-const app = express();
-const { v4: uuidv4 } = require("uuid");
-const io = require("socket.io");
-const jade = require("ejs");
-app.use(express.static("public"));
-app.engine('html', require('ejs').renderFile);
+const express = require("express")
+const passport = require("passport")
+const app = express()
+const io = require("socket.io")
+const jade = require("ejs")
+app.use(express.static("public"))
+app.engine("html", require("ejs").renderFile)
 
-
-app.post("/login", passport.authenticate("local"), function(req, res){
-    res.redirect("/users/" + req.user.username);
-});
-
+app.post("/login", passport.authenticate("local"), function (req, res) {
+  res.redirect("/users/" + req.user.username)
+})
 
 app.get("/", (req, res) => {
-    res.render('index.html');
-});
+  res.render("index.html")
+})
 
 app.get("/login", (req, res) => {
-    res.render('login.html');
-});
+  res.render("login.html")
+})
 
-app.listen(80);
+app.listen(80)
